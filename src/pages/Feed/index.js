461c6b1295aebc691783f44a91ca1ec08bb08f3f
Ariane Mateus <ariane.ccomp@gmail.com>
2020-07-24 16:49:23 -0300

    feat: pagination

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useCallback, useEffect} from 'react';
+import {View, FlatList} from 'react-native';
 
-// import { Container } from './styles';
+import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
 
 const Feed = () => {
-  return <View />;
+  const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  const loadPage = async (pageNumber = page) => {
+    if (total && pageNumber > total) {
+      return;
+    }
+    console.log(pageNumber);
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+    );
+    const data = await response.json();
+
+    const totalItems = response.headers.get('X-Total-Count');
+    setTotal(Math.ceil(totalItems / 5));
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+  };
+
+  useEffect(() => {
+    loadPage();
+  }, []);
+
+  return (
+    <View>
+      <FlatList
+        data={feed}
+        keyExtractor={(post) => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        renderItem={({item}) => (
+          <Post>
+            <Header>
+              <Avatar source={{uri: item.author.avatar}} />
+              <Name>{item.author.name}</Name>
+            </Header>
+
+            <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
+
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </View>
+  );
 };
 
 export default Feed;
