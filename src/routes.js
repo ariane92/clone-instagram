4908a977c4f1f3cc9317625aa0daa33028eb7903
Ariane Mateus <ariane.ccomp@gmail.com>
2020-07-24 10:47:11 -0300

    feat: Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Routes = createAppContainer(createStackNavigator({}));
