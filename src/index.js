f6e3759e8b37080da4d7514f73f02f97eeb4d75f
Ariane Mateus <ariane.ccomp@gmail.com>
2020-07-24 15:31:43 -0300

    feat: Header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {View} from 'react-native';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
-  return <View />;
-}
+import Routes from './routes';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor="#f5f5f5" />
+      {<Routes />}
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,24 @@
 import React from 'react';
-import {createAppContainer} from 'react-navigation';
+import {Image} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-const Routes = createAppContainer(createStackNavigator({}));
+import logo from './assets/instagram.png';
+import Feed from './pages/Feed';
+const App = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <App.Navigator
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerTitle: <Image source={logo} />,
+        headerStyle: {
+          backgroundColor: '#f5f5f5',
+        },
+      }}>
+      <App.Screen name="Feed" component={Feed} />
+    </App.Navigator>
+  );
+};
+
+export default Routes;
